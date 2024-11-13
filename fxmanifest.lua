8bf9ac003e089518b661b4a7121b0351c7d73569
Jake Murray <j1993murray@hotmail.com>
2024-11-13 23:50:58 +0000

    Update: Rename file & update manifest
    - Keep it same name across all resources
    - Set manifest to new standard

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
-author 'Vibrant'
-description 'Lumberjack job'
+name 'vib_lumberjack'
+author 'Vibrant Resources'
 version '1.0.0'
+repository 'https://github.com/VibrantResources/vib_lumberjack'
+description 'A lumberjack job designed for civilians providing tress will individual cooldowns, vehicle spawning and placeable objects for plan creation'
 
 client_scripts {
     'client/*.lua',
